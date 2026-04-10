25a9ba0598f86f9852fd03bc4363156a80d0c773
Adarsh <ankuclash2@gmail.com>
2026-04-10 13:47:12 +0530

    multimodel,endpoints
    updated to huggingface endpoint embeddings
    
    updated to use llama-3.1 model

--- chatbot.py
+++ chatbot.py
@@ -1,44 +1,65 @@
+import os
+from dotenv import load_dotenv
 from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
-from transformers import pipeline
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from langchain_groq import ChatGroq
+
+load_dotenv()
+hf_token = os.getenv("HF_TOKEN")
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+if not hf_token:
+    raise ValueError("HF_TOKEN not found!")
+if not groq_api_key:
+    raise ValueError("GROQ_API_KEY not found!")
 
 DB_DIRECTORY = "./website_db"
-EMBEDDING_MODEL = "all-MiniLM-L6-v2"
 
-embedding = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
+print("Loading Database and Embeddings...")
+embedding = HuggingFaceEndpointEmbeddings(
+    model="sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=hf_token
+)
 
 db = Chroma(
     persist_directory=DB_DIRECTORY,
     embedding_function=embedding
 )
 
-# Local model
-generator = pipeline(
-    "text-generation",
-    model="google/flan-t5-base",
-    max_length=512
+print("Connecting to Groq API...")
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+    api_key=groq_api_key,
+    temperature=0.3 
 )
 
 def retrieve_context(query: str, k: int = 3) -> str:
     docs = db.similarity_search(query, k=k)
     return "\n\n".join([doc.page_content for doc in docs])
 
-
 def generate_answer(query: str, context: str) -> str:
     prompt = f"""
-Answer the question ONLY using the provided context.
-If the answer is not in the context, say "I don't know".
-
-Context:
-{context}
-
-Question:
-{query}
-"""
-    result = generator(prompt)[0]["generated_text"]
-    return result
-
+    Answer the question ONLY using the provided context.
+    If the answer is not in the context, say "I can help you with company related informations.Thank You!".
+    
+    Context:
+    {context}
+    
+    Question:
+    {query}
+    """
+    
+    result = llm.invoke(prompt)
+    return result.content
 
 def chat(query: str) -> str:
+    print(f"\nUser: {query}")
     context = retrieve_context(query)
-    return generate_answer(query, context)
+    answer = generate_answer(query, context)
+    return answer
+
+#  Test
+if __name__ == "__main__":
+    test_question = "What is your name"
+    response = chat(test_question)
+    print(f"\nBot: {response}\n")
